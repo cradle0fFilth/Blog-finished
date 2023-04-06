f621dfb7713fd76cb1402bee0333b3f2ad11e1c4
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-04-06 15:35:43 +0800

    use to initial a blog

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+import { formatISO9075 } from 'date-fns';
+export default function Post({ title, summary, cover, createdAt, author }) {
+  return (
+    <div className='post'>
+      <div className='image'>
+        <img src={'http://localhost:4000/' + cover}></img>
+      </div>
+      <div className='texts'>
+        <h2>{title}</h2>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>)
+}
