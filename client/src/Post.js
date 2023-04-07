8b4c75ba0052393e57ebd1ebbfde9d263be86748
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-04-07 16:35:33 +0800

    Link to postPage

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-import { formatISO9075 } from 'date-fns';
-export default function Post({ title, summary, cover, createdAt, author }) {
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
   return (
-    <div className='post'>
-      <div className='image'>
-        <img src={'http://45.77.23.190:4000/' + cover}></img>
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://45.77.23.190:4000/'+cover} alt=""/>
+        </Link>
       </div>
-      <div className='texts'>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
         <h2>{title}</h2>
+        </Link>
         <p className="info">
           <a className="author">{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className="summary">{summary}</p>
       </div>
-    </div>)
+    </div>
+  );
 }
